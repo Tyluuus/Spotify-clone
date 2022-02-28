652895e4a18215f47320b7bc5fa8b793838f7082
Tyluuus <tylus97@gmail.com>
2022-02-28 19:29:58 +0100

    Developing browsing. Added ability to open playlist, check content within it. Added buttons to play and share playlist

---
+++ SpotifyClone/ViewModels/PlaylistHeaderViewViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  PlaylistHeaderViewViewModel.swift
+//  SpotifyClone
+//
+//  Created by Piotr Tyl on 28/02/2022.
+//
+
+import Foundation
+
+struct PlaylistHeaderViewViewModel {
+    let name: String?
+    let ownerName: String?
+    let description: String?
+    let artworkURL: URL?
+}
