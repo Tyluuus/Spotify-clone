0194e3ba4fd48f4690d8c8eb47b99bb1e7476b99
Tyluuus <tylus97@gmail.com>
2022-03-09 21:47:29 +0100

    Library playlists part

---
+++ SpotifyClone/Models/LibraryPlaylistsResponse.swift
@@ -0,0 +1,12 @@
+//
+//  LibraryPlaylistsResponse.swift
+//  SpotifyClone
+//
+//  Created by Piotr Tyl on 09/03/2022.
+//
+
+import Foundation
+
+struct LibraryPlaylistsResponse: Codable {
+    let items: [Playlist]
+}
